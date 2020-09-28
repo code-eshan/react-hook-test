3bec24e40034db3e9878593c5b98a9d8f3ca20fd
Labeeshan <labeeshanr@gmail.com>
2020-09-28 13:51:10 +0530

    useEffect update

--- hooks/src/Components/Effect Hook/HookCounterOne.js
+++ hooks/src/Components/Effect Hook/HookCounterOne.js
@@ -1,20 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function HookCounterOne() {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     //This is an alternate to componentDidMount() and componentDidUpdate()
     //useEffect runs after every render of the component
     useEffect(() => {
         //We can use backticks to define a string in which if it contains a
         //javascript function
+        console.log('useEffect Called...')
         document.title = `You clicked ${count} times`
-    })
+
+        //As the second parameter, in an array, we can state what change we need
+        //to watch for instead of calling useEffect whenever the changes are maded all
+        //the time
+    }, [count])
 
     return (
         <div>
-            <button onClick={() => {setCount(count + 1)}}>Click {count} times</button>
+            <input type="text" value={name} onChange={e => {setName(e.target.value)}}/>
+            <button onClick={() => { setCount(count + 1) }}>Click {count} times</button>
         </div>
     )
 }
